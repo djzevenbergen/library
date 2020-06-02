25f367945e3052f3a4b49cb4e1b097785f23f950
nitunwa <nitun.2@gmail.com>
2020-06-02 10:20:33 -0700

    add initial commit

---
+++ Library/Models/Book.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace Library.Models
+{
+  public class Book
+  {
+    public Book()
+    {
+      this.Categories = new HashSet<AuthorBook>();
+    }
+
+    public int BookId { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public virtual ApplicationUser User { get; set; }
+
+    public ICollection<AuthorBook> Authors { get; }
+  }
+}
